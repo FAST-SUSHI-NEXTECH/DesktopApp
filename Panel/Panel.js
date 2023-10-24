703c3f0dd81c1eb1f9bcfe568749e8299eea4da7
Luckyluck <denislucas1a@gmail.com>
2023-10-24 11:08:27 +0200

    new file:   Login/preload.js 	modified:   Panel/Panel.js 	modified:   Panel/index.html 	new file:   Panel/preload.js

--- Panel/Panel.js
+++ Panel/Panel.js
@@ -1,56 +1,44 @@
-const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+// main.js
 
-sideLinks.forEach(item => {
-    const li = item.parentElement;
-    item.addEventListener('click', () => {
-        sideLinks.forEach(i => {
-            i.parentElement.classList.remove('active');
-        })
-        li.classList.add('active');
-    })
-});
+// Modules pour la gestion de l'appli et la création de la BrowserWindow native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
-const menuBar = document.querySelector('.content nav .bx.bx-menu');
-const sideBar = document.querySelector('.sidebar');
-
-menuBar.addEventListener('click', () => {
-    sideBar.classList.toggle('close');
-});
-
-const searchBtn = document.querySelector('.content nav form .form-input button');
-const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
-const searchForm = document.querySelector('.content nav form');
-
-searchBtn.addEventListener('click', function (e) {
-    if (window.innerWidth < 576) {
-        e.preventDefault;
-        searchForm.classList.toggle('show');
-        if (searchForm.classList.contains('show')) {
-            searchBtnIcon.classList.replace('bx-search', 'bx-x');
-        } else {
-            searchBtnIcon.classList.replace('bx-x', 'bx-search');
-        }
+const createWindow = () => {
+  // Création de la browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
     }
-});
+  })
 
-window.addEventListener('resize', () => {
-    if (window.innerWidth < 768) {
-        sideBar.classList.add('close');
-    } else {
-        sideBar.classList.remove('close');
-    }
-    if (window.innerWidth > 576) {
-        searchBtnIcon.classList.replace('bx-x', 'bx-search');
-        searchForm.classList.remove('show');
-    }
-});
+  // et chargement de l'index.html de l'application.
+  mainWindow.loadFile('index.html')
 
-const toggler = document.getElementById('theme-toggle');
+  // Ouvrir les outils de développement.
+  // mainWindow.webContents.openDevTools()
+}
 
-toggler.addEventListener('change', function () {
-    if (this.checked) {
-        document.body.classList.add('dark');
-    } else {
-        document.body.classList.remove('dark');
-    }
-});
+// Cette méthode sera appelée quand Electron aura fini
+// de s'initialiser et sera prêt à créer des fenêtres de navigation.
+// Certaines APIs peuvent être utilisées uniquement quant cet événement est émit.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // Sur macOS il est commun de re-créer une fenêtre  lors 
+    // du click sur l'icone du dock et qu'il n'y a pas d'autre fenêtre ouverte.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quitter quand toutes les fenêtres sont fermées, sauf sur macOS. Dans ce cas il est courant
+// que les applications et barre de menu restents actives jusqu'à ce que l'utilisateur quitte 
+// de manière explicite par Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// Dans ce fichier vous pouvez inclure le reste du code spécifique au processus principal. Vous pouvez également le mettre dans des fichiers séparés et les inclure ici.
