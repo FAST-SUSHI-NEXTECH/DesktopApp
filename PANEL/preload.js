e328e487ec966e6bf3ba296656eb874f9e7588a0
Luckyluck <denislucas1a@gmail.com>
2023-11-20 10:00:15 +0100

    new file:   PANEL/admin-menus.html 	new file:   PANEL/commandes-cours.html 	new file:   PANEL/css/admin.css 	new file:   PANEL/css/client.css 	new file:   PANEL/css/commande.css 	new file:   PANEL/css/ingredient.css 	new file:   PANEL/css/parametres.css 	new file:   PANEL/css/style.css 	new file:   PANEL/css/ticket.css 	new file:   PANEL/gestion-ingredients.html 	new file:   PANEL/image/logo_blanc.png 	modified:   PANEL/index.html 	new file:   PANEL/liste-clients.html 	modified:   PANEL/main.js 	new file:   PANEL/parametres.html 	modified:   PANEL/preload.js 	new file:   PANEL/ticket.html

--- PANEL/preload.js
+++ PANEL/preload.js
@@ -1,7 +1,7 @@
 // preload.js
 
-// Toutes les API Node.js sont disponibles dans le processus de préchargement.
-// Il a la même sandbox qu'une extension Chrome.
+// All the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
