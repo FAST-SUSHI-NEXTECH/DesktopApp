c907bb08b4f4dccca450dca247dfd97aee87b70d
buggluck <emiliendjafeur@gmail.com>
2023-12-19 11:04:31 +0100

    modified:   a_js/Api.js 	modified:   html/Historique-commande.html 	modified:   html/admin-menus.html 	modified:   html/gestion-ingredients.html 	modified:   html/index.html 	modified:   html/parametres.html 	modified:   html/ticket.html 	modified:   main.js

--- a_js/Api.js
+++ a_js/Api.js
@@ -1,14 +1,15 @@
-// Nouvelle fonction pour charger les données depuis l'API
-async function chargerDonneesDepuisAPI() {
+// Api.js
+
+async function chargerDonneesDepuisAPI(webContents) {
     const urlAPI = 'http://185.255.112.208:3000/user/client';
-    console.log("TOTO")
+    const authToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImNoaWVmIiwiaWF0IjoxNzAyODg2NzMwLCJleHAiOjE3MDI5NzMxMzB9.u6KqvrnMkIkCdHTSYhonDjZxSXdqIXC3eMAPv42GSvY';
 
     try {
         const reponse = await fetch(urlAPI, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
-                'Autorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImNoaWVmIiwiaWF0IjoxNzAyODg2NzMwLCJleHAiOjE3MDI5NzMxMzB9.u6KqvrnMkIkCdHTSYhonDjZxSXdqIXC3eMAPv42GSvY'
+                'Authorization': authToken,
             },
         });
 
@@ -17,37 +18,14 @@ async function chargerDonneesDepuisAPI() {
         }
 
         const donnees = await reponse.json();
+        console.log('Data from API:', donnees);
 
-        // Remplacez cette étape par le code nécessaire pour mettre à jour votre interface utilisateur
-        mettreAJourInterfaceUtilisateur(donnees);
+        // Update the user interface in the renderer process
+        webContents.send('update-data', donnees);
 
     } catch (erreur) {
         console.error('Une erreur s\'est produite lors de la récupération des données:', erreur);
     }
 }
-
-// Fonction pour mettre à jour l'interface utilisateur avec les données de l'API
-// Fonction pour mettre à jour l'interface utilisateur avec les données de l'API
-function mettreAJourInterfaceUtilisateur(donnees) {
-    const tbody = document.querySelector('#client-list');  // Corrected selector
-
-    // Efface le contenu existant du tableau
-    tbody.innerHTML = '';
-
-    // Ajoute chaque utilisateur au tableau
-    donnees.forEach(utilisateur => {
-        const row = document.createElement('tr');
-        row.innerHTML = `<td>${utilisateur.id}</td><td>${utilisateur.last_name} ${utilisateur.first_name}</td><td>${utilisateur.email}</td>`;
-        tbody.appendChild(row);
-    });
-}
-
-
-// Appel de la fonction pour charger les données au chargement de la page
-chargerDonneesDepuisAPI();
-
-// Ajouter un écouteur d'événements au lien Liste Client
-const listeClientsLink = document.getElementById('listeClientsLink');
-listeClientsLink.addEventListener('click', chargerDonneesDepuisAPI);
 
 module.exports = { chargerDonneesDepuisAPI };
--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
+// main.js
+
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+const fetch = require('node-fetch'); // Import fetch correctly
+const { chargerDonneesDepuisAPI } = require('./a_js/Api'); // Adjust the path accordingly
 
 const createWindow = () => {
-  // Permet de faire le tab
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+    // Enable tabs
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
 
-  mainWindow.loadFile('./html/Log.html')
-  
-}
+    mainWindow.loadFile('./html/Log.html');
+
+    // Call the imported function
+    chargerDonneesDepuisAPI(mainWindow.webContents); // Pass webContents to the function
+};
+
 app.whenReady().then(() => {
-  createWindow()
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
