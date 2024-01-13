246904d843d1d545d6690bcbcb04143bdc318a11
Luckyluck <denislucas1a@gmail.com>
2024-01-13 13:56:23 +0100

    deleted:    .gitignore 	deleted:    API/API_DELETE.js 	deleted:    API/API_GET.js 	deleted:    API/API_POST.js 	deleted:    API/API_PUT.js 	deleted:    Main/Log.js 	deleted:    Main/preload.js 	deleted:    Main/renderer.js 	deleted:    README.md     deleted:    css/Log.css 	deleted:    css/admin.css 	deleted:    css/client.css 	deleted:    css/commande.css 	deleted:    css/ingredient.css 	deleted:    css/parametres.css 	deleted:    css/style.css 	deleted:    css/ticket.css 	deleted:    interface/Log.html 	deleted:    interface/admin-menus.html 	deleted:    interface/commandes-cours.html 	deleted:    interface/gestion-ingredients.html 	deleted:    interface/index.html 	deleted:    interface/liste-clients.html 	deleted:    interface/parametres.html 	deleted:    interface/ticket.html 	deleted:    main.js

--- Main/renderer.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('logout').addEventListener('click', closeWindow);
-
-function closeWindow() {
-    let newWindow = open(location, '_self');
-    //location permet d'idendifier le path pour que le bouton puisse s'exécuter
-    newWindow.close();
-}
