61655693769693299afdad5b327da5e51e68e296
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-01-26 14:05:58 +0100

    Actualisation -b emilien

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { app, BrowserWindow } from 'electron';
 import { join } from 'path';
-import Api from './a_js/Api_get.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -8,7 +7,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const createWindow = () => {
-    // Enable tabs
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -17,9 +15,7 @@ const createWindow = () => {
         },
     });
 
-    mainWindow.loadFile('./html/Log.html');
-
-    Api.chargerDonneesDepuisAPI(mainWindow.webContents);
+    mainWindow.loadFile('./src/conf/ui/Log.html');
 };
 
 app.whenReady().then(() => {
---
+++ src/request/API_GET.js
@@ -0,0 +1,66 @@
+fetch('../../../config.json')
+  .then(response => response.json())
+  .then(config => {
+    fetch(`${config.url_client}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${config.token}`,
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      const clientList = document.getElementById('client-list');
+      data.forEach((client) => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td>${client.id_user}</td>
+        <td>${client.last_name}</td>
+        <td>${client.first_name}</td>
+        <td>${client.tel}</td>
+        <td>${client.email}</td>
+        <td>${client.username}</td>
+        `;
+        clientList.appendChild(tr);
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+fetch('../../../config.json')
+  .then(response => response.json())
+  .then(config => {
+    fetch(`${config.url_order}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${config.token}`,
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      const orderList = document.getElementById('order-list');
+      data.forEach((order) => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td>${order.id_order}</td>
+        <td>${order.id_client}</td>
+        <td>${order.id_picker}</td>
+        <td>${order.order_state}</td>
+        <td>${order.date}</td>
+        `;
+        orderList.appendChild(tr);
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
