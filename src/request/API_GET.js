903c6b116b967d045dbb3e5c59daeaf3738d0295
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-01-26 16:26:41 +0100

    modif

--- src/request/API_GET.js
+++ src/request/API_GET.js
@@ -64,3 +64,36 @@ fetch('../../../config.json')
   .catch((error) => {
     console.error('Error:', error);
   });
+
+  fetch('../../../config.json')
+  .then(response => response.json())
+  .then(config => {
+    fetch(`${config.url_client}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${config.token}`,
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      const orderList = document.getElementById('order-list');
+      data.forEach((order) => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td>${order.id_order}</td>
+        <td>${order.id_client}</td>
+        <td>${order.id_picker}</td>
+        <td>${order.order_state}</td>
+        <td>${order.date}</td>
+        `;
+        orderList.appendChild(tr);
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
