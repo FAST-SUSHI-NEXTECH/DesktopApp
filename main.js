a1a469b8daa408e4f4a01aff910c22869a526b4b
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-01-26 14:10:02 +0100

    Actualisation main

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const createWindow = () => {
-  // Permet de faire le tab
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: join(__dirname, 'preload.js'),
+        },
+    });
 
-  mainWindow.loadFile('./html/Log.html')
-}
+    mainWindow.loadFile('./src/conf/ui/Log.html');
+};
+
 app.whenReady().then(() => {
-  createWindow()
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
---
+++ renderer.js
@@ -0,0 +1,23 @@
+document.getElementById('logout').addEventListener('click', closeWindow);
+
+function closeWindow() {
+    let newWindow = open(location, '_self');
+    newWindow.close();
+}
+
+import { ipcRenderer } from 'electron';
+
+ipcRenderer.on('update-data', (event, donnees) => {
+  const clientList = document.getElementById('client-list');
+
+  donnees.forEach((client) => {
+    const row = document.createElement('tr');
+     const columns = ['id_user', 'last_name', 'first_name', 'tel', 'email'];
+    columns.forEach((column) => {
+      const cell = document.createElement('td');
+      cell.textContent = client[column];
+      row.appendChild(cell);
+    });
+    clientList.appendChild(row);
+  });
+});
