760d0b269aba078fe1e9d1eb6dda660a6b149525
buggluck <emiliendjafeur@gmail.com>
2024-02-20 11:32:13 +0100

    update delete config

--- config.js
+++
@@ -1,7 +0,0 @@
-export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTEsInVzZXJuYW1lIjoiZW1pbGllbiIsInBlcm0iOjMsImlhdCI6MTcwODQxNTEwNSwiZXhwIjoxNzA4NTAxNTA1fQ.L-hMtSAqWAAdI_swVAb5jnwMp8ugfvZ0UvU6FEQ_bkk";
-export const url_client = "http://185.255.112.208:3000/user/client";
-export const ip = "http://185.255.112.208:3000/login";
-export const url_order = "http://185.255.112.208:3000/order";
-export const url_delete = "http://185.255.112.208:3000/user/delete";
-export const username = "emilien";
-export const password = "emilien";
